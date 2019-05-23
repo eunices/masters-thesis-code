59c8265ffbc480008826c8ff28f645965b030ff9
Eunice Soh <t0915817@kelaberetiv.sg>
2019-05-23 10:45:15 +0800

    [EJYS] Add a bash script for backing up data to Dropbox

---
+++ backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Backup files
+
+# Backup data to Dropbox
+d=$(date +%Y-%m-%d)
+echo Today is "$d"
+
+fn=msc-thesis-code-data-$d.zip
+echo "$fn"
+zip -r $fn data/*
+echo File is backed up as $fn
+
+dir="../../Dropbox/msc-thesis/data-dump"
+mv $fn $dir/$fn
