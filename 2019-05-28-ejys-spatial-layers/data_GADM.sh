3cb84d4fd20fb3f94a70e95d1ee2f7899c788297
Eunice Soh <t0915817@kelaberetiv.sg>
2019-05-30 16:16:11 +0800

    [EJYS] Adding more comments for data_GADM

--- 2019-05-28-ejys-spatial-layers/data_GADM.sh
+++ 2019-05-28-ejys-spatial-layers/data_GADM.sh
@@ -20,16 +20,17 @@ unzip -o "*.zip"
 rm *.zip
 
 echo "Reorganizing files"
-cd $dir
+
+cd $dir # creating folder for all primary divisions (countries)
 shp_pri_dir="shp_pri"
 mkdir $shp_pri_dir
-mv *_0.* "$shp_pri_dir/"
-cd $shp_pri_dir && rename -f 's/_ABW_0/\_0/' *_ABW_0* && cd ".."
+mv *_0.* "$shp_pri_dir/" #
+cd $shp_pri_dir && rename -f 's/_ABW_0/\_0/' *_ABW_0* && cd ".." # copying file to be merged
 
-shp_sec_dir="shp_sec"
+shp_sec_dir="shp_sec" # creating folder for all sec divisions
 mkdir $shp_sec_dir
 mv *_1.* "$shp_sec_dir/"
-cd $shp_sec_dir && rename -f 's/_AFG_1/\_1/' *_AFG_1* && cd ".."
+cd $shp_sec_dir && rename -f 's/_AFG_1/\_1/' *_AFG_1* && cd ".." # copying file to be merged
 
 
 echo "Running script to merge shp_pri" # can be extended for shp_sec later
