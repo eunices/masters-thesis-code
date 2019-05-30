747d054b8b7cc27d7815958e2b87eb0f76d866dd
Eunice Soh <t0915817@kelaberetiv.sg>
2019-05-30 18:45:05 +0800

    [EJYS] Improve chelsea bash script to organise folders better

--- 2019-05-28-ejys-spatial-layers/process_chelsa.sh
+++ 2019-05-28-ejys-spatial-layers/process_chelsa.sh
@@ -2,7 +2,9 @@ base_dir=${PWD%/*}
 geo_data_dir="$base_dir/data/geo"
 dir="$geo_data_dir/chelsa/bioclim"
 files="$dir/*.tif"
+asc_subfolder="asc"
 
+# Convert from geotiff to asc
 cd $dir
 for f in $files
 do 
@@ -11,3 +13,7 @@ do
     echo $filename
     gdal_translate -of GTiff $filename $filename.asc; 
 done
+
+# Move files to subfolder
+mkdir $asc_subfolder
+mv -f *.asc "$asc_subfolder"
