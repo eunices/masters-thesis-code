91828182caae348380568376b47a86aa6f9cf597
Eunice Soh <eunice.sjy@gmail.com>
2019-05-31 21:50:42 +0800

    [EJYS] Modify activation of conda env

--- 2019-05-28-ejys-spatial-layers/data_GADM.sh
+++ 2019-05-28-ejys-spatial-layers/data_GADM.sh
@@ -4,7 +4,7 @@ geo_data_dir="$base_dir/data/geo"
 dir="$geo_data_dir/gadm"
 folder_dir=$(pwd)
 
-source activate msc
+conda activate msc
 
 echo "Scraping urls of specified file type"
 python -m data_GADM
--- 2019-05-28-ejys-spatial-layers/data_chelsa_others.sh
+++ 2019-05-28-ejys-spatial-layers/data_chelsa_others.sh
@@ -6,7 +6,7 @@ gmted_dir="$geo_data_dir/gmted2010"
 beck_dir="$geo_data_dir/beck_koppen"
 folder_dir=$(pwd)
 
-source activate msc
+conda activate msc
 
 echo "Making all dirs"
 mkdir $geo_data_dir
