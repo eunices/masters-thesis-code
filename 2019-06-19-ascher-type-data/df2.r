17a2e9ce97dee47ad5c53d858063742c2f54933a
Eunice Soh <eunice.sjy@gmail.com>
2019-08-27 09:27:07 +0800

    [EJYS] Remove word edit

--- 2019-06-19-ascher-type-data/df2.r
+++ 2019-06-19-ascher-type-data/df2.r
@@ -95,7 +95,7 @@ for (i in 1:dim(describers_info)[1]) {
     print(paste0("Row ", i , " completed for ", idx_row))
 }
 
-write.csv(describers, paste0(dir, "2019-05-23-Apoidea world consensus file Sorted by name 2019 filtered_3.2.0-describers_edit2.csv"), na='', row.names=F, fileEncoding="UTF-8")
+write.csv(describers, paste0(dir, "2019-05-23-Apoidea world consensus file Sorted by name 2019 filtered_3.2.0-describers2.csv"), na='', row.names=F, fileEncoding="UTF-8")
 
 
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
@@ -103,7 +103,7 @@ write.csv(describers, paste0(dir, "2019-05-23-Apoidea world consensus file Sorte
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 print(paste0(Sys.time(), " --- 'describers': summarize by idx"))
 
-describers <- fread(paste0(dir, "2019-05-23-Apoidea world consensus file Sorted by name 2019 filtered_3.2.0-describers_edit2.csv"), na.strings=c('', 'NA'), encoding="UTF-8", quote='"')
+describers <- fread(paste0(dir, "2019-05-23-Apoidea world consensus file Sorted by name 2019 filtered_3.2.0-describers2.csv"), na.strings=c('', 'NA'), encoding="UTF-8", quote='"')
 
 # Merge back the other columns
 describers_merged <- data.table(describers)
@@ -138,7 +138,7 @@ describers_idx <- data.table(describers_idx)
 describers_idx <- describers_idx[order(full.name.of.describer.n),]
 describers_idx$idx_auth <- 1:dim(describers_idx)[1]
 
-write.csv(describers_idx, paste0(dir, "2019-05-23-Apoidea world consensus file Sorted by name 2019 filtered_3.2.1-describers_edit.csv"), na='', row.names=F, fileEncoding="UTF-8")
+write.csv(describers_idx, paste0(dir, "2019-05-23-Apoidea world consensus file Sorted by name 2019 filtered_3.2.1-describers.csv"), na='', row.names=F, fileEncoding="UTF-8")
 
 
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
@@ -146,7 +146,7 @@ write.csv(describers_idx, paste0(dir, "2019-05-23-Apoidea world consensus file S
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 print(paste0(Sys.time(), " --- 'describers': further cleaning"))
 
-describers <- fread(paste0(dir, "2019-05-23-Apoidea world consensus file Sorted by name 2019 filtered_3.2.1-describers_edit.csv"),
+describers <- fread(paste0(dir, "2019-05-23-Apoidea world consensus file Sorted by name 2019 filtered_3.2.1-describers.csv"),
             na.strings=c('', 'NA'), encoding="UTF-8", quote='"')
 describers$describer.gender.n <- factor(describers$describer.gender.n, levels=c("F", "M", "U"), ordered=T)
 describers <- describers[order(full.name.of.describer.n, describer.gender.n)]
@@ -155,7 +155,7 @@ describers <- describers[!duplicated(full.name.of.describer.n)]
 dim(describers)
 describers$describer.gender.n <- as.character(describers$describer.gender.n)
 
-write.csv(describers, paste0(dir, "2019-05-23-Apoidea world consensus file Sorted by name 2019 filtered_3.2.2-describers_edit.csv"), na='', row.names=F, fileEncoding="UTF-8")
+write.csv(describers, paste0(dir, "2019-05-23-Apoidea world consensus file Sorted by name 2019 filtered_3.2.2-describers.csv"), na='', row.names=F, fileEncoding="UTF-8")
 
 
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
@@ -163,7 +163,7 @@ write.csv(describers, paste0(dir, "2019-05-23-Apoidea world consensus file Sorte
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 print(paste0(Sys.time(), " --- 'describers': further denormalization of data"))
 
-describers <- fread(paste0(dir, "2019-05-23-Apoidea world consensus file Sorted by name 2019 filtered_3.2.2-describers_edit.csv"),
+describers <- fread(paste0(dir, "2019-05-23-Apoidea world consensus file Sorted by name 2019 filtered_3.2.2-describers.csv"),
             na.strings=c('', 'NA'), encoding="UTF-8", quote='"')
 
 # Dataset unnested from idxes
@@ -173,9 +173,9 @@ describers <- describers %>%
 
 describers <- data.table(describers)
 
-write.csv(describers, paste0(dir, "2019-05-23-Apoidea world consensus file Sorted by name 2019 filtered_3.2.3-describers_edit.csv"), na='', row.names=F, fileEncoding="UTF-8")
+write.csv(describers, paste0(dir, "2019-05-23-Apoidea world consensus file Sorted by name 2019 filtered_3.2.3-describers.csv"), na='', row.names=F, fileEncoding="UTF-8")
 
-describers <- fread(paste0(dir, "2019-05-23-Apoidea world consensus file Sorted by name 2019 filtered_3.2.3-describers_edit.csv"),
+describers <- fread(paste0(dir, "2019-05-23-Apoidea world consensus file Sorted by name 2019 filtered_3.2.3-describers.csv"),
             na.strings=c('', 'NA'), encoding="UTF-8", quote='"')
 describers$idxes <- as.numeric(describers$idxes)
 
@@ -188,7 +188,7 @@ describers <- unique(describers)
 describers$idxes <- as.numeric(describers$idxes)
 describers <- describers[order(idxes)]
 
-write.csv(describers, paste0(dir, "2019-05-23-Apoidea world consensus file Sorted by name 2019 filtered_3.2.4-describers_edit.csv"), na='', row.names=F, fileEncoding="UTF-8")
+write.csv(describers, paste0(dir, "2019-05-23-Apoidea world consensus file Sorted by name 2019 filtered_3.2.4-describers.csv"), na='', row.names=F, fileEncoding="UTF-8")
 
 # https://stackoverflow.com/questions/13773770/
 
@@ -200,9 +200,9 @@ print(paste0(Sys.time(), " --- 'describers': joining author data back to main da
 
 df <- fread(paste0(dir, "2019-05-23-Apoidea world consensus file Sorted by name 2019 filtered_3.1-useful-col.csv"), integer64='character', na.strings=c('', 'NA'), encoding='UTF-8')
 
-describers <- fread(paste0(dir, "2019-05-23-Apoidea world consensus file Sorted by name 2019 filtered_3.2.4-describers_edit.csv"), integer64='character', na.strings=c('', 'NA'), encoding='UTF-8')
+describers <- fread(paste0(dir, "2019-05-23-Apoidea world consensus file Sorted by name 2019 filtered_3.2.4-describers.csv"), integer64='character', na.strings=c('', 'NA'), encoding='UTF-8')
 
-describers2 <- fread(paste0(dir, "2019-05-23-Apoidea world consensus file Sorted by name 2019 filtered_3.2.1-describers_edit.csv"), integer64='character', na.strings=c('', 'NA'), encoding='UTF-8')
+describers2 <- fread(paste0(dir, "2019-05-23-Apoidea world consensus file Sorted by name 2019 filtered_3.2.1-describers.csv"), integer64='character', na.strings=c('', 'NA'), encoding='UTF-8')
 
 df$idx <- as.numeric(df$idx)
 describers$idx <- as.numeric(describers$idx)
