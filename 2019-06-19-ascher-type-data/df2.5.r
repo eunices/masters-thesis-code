d9f8f89dfa5f3930b4118cc41c4cfcdab836d088
Eunice Soh <eunice.sjy@gmail.com>
2019-09-05 14:29:26 +0800

    [EJYS] Correct to filename

--- 2019-06-19-ascher-type-data/df2.5.r
+++ 2019-06-19-ascher-type-data/df2.5.r
@@ -21,6 +21,7 @@ des[, names(des) := lapply(.SD, function(x) gsub('\\"\\"', '\\"', x))] # fread d
 
 # Create pairs
 ps2 <- ps[grepl(";", full.name.of.describer),]
+ps4 <- ps[!grepl(";", full.name.of.describer),]
 ps2 <- strsplit(ps2$full.name.of.describer, split = "; ")
 ps2 <- lapply(ps2, function(x) as.data.frame(t(combn(x, m=2))))
 ps2 <- rbindlist(ps2); names(ps2) <- c('p1', 'p2')
@@ -29,16 +30,20 @@ ps2 <- rbindlist(ps2); names(ps2) <- c('p1', 'p2')
 
 # Count pairs
 ps2 <- ps2[, .N, by=c("p1", "p2")][order(N)]
+ps4 <- ps4[, c("full.name.of.describer")]; names(ps4) <- "p1"
+ps4 <- ps4[, .N, by=c("p1")]; ps4$p2 <- NA
+ps2 <- rbind(ps2, ps4)
 
 # Join with author information
 cols <- c('idx_auth', 'full.name.of.describer.n', 'describer.gender.n',
           'origin.country.describer.n', 'residence.country.describer.first', 'spp_N')
 de <- des[,..cols]
-ps3 <- merge(ps2, de, by.x='p1', by.y='full.name.of.describer.n')
-ps3 <- merge(ps3, de, by.x='p2', by.y='full.name.of.describer.n', suffixes=c('_p1', '_p2'))
+ps3 <- merge(ps2, de, by.x='p1', by.y='full.name.of.describer.n', all.x=T, all.y=F)
+ps3 <- merge(ps3, de, by.x='p2', by.y='full.name.of.describer.n', suffixes=c('_p1', '_p2'), all.x=T, all.y=F)
 
 p1_names <- names(ps3)[grepl("_p1", names(ps3))]
 p2_names <- names(ps3)[grepl("_p2", names(ps3))]
 col_order <- c("N", "p1", "p2", p1_names, p2_names)
 
-write.csv(ps3[order(-N), ..col_order], paste0(dir, "2019-05-23-Apoidea world consensus file Sorted by name 2019 descrips3bers_7.0-author-networks.csv"), na='', row.names=F, fileEncoding="UTF-8")
+
+write.csv(ps3[order(-N), ..col_order], paste0(dir, "2019-05-23-Apoidea world consensus file Sorted by name 2019 describers_7.0-author-networks.csv"), na='', row.names=F, fileEncoding="UTF-8")
