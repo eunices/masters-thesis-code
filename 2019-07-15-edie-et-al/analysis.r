48ce6b32d2094b4f8e9c8b3717736f2c8cc90609
eunices <eunice.sjy@gmail.com>
2019-10-10 23:04:46 +0800

    [EJYS] Add todo

--- 2019-07-15-edie-et-al/analysis.r
+++ 2019-07-15-edie-et-al/analysis.r
@@ -3,9 +3,9 @@ print("WELCOME TO ANALYSES SCRIPTS FOR BEE TYPE DATA")
 print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
 
-dir_script_ed <- "2019-07-15-edie-et-al/"
-setwd("C:/Dev/msc-thesis-code/")
+# setwd("C:/Dev/msc-thesis-code/")
 # source(paste0(dir_script_ed, "analysis.r"))
+dir_script_ed <- "2019-07-15-edie-et-al/"
 
 # Scripts
 #############
@@ -16,3 +16,5 @@ source(paste0(dir_script_ed, 'analysis2.r')) # model fitting
 source(paste0(dir_script_ed, 'analysis3.r')) # post
 source(paste0(dir_script_ed, 'analysis4.r')) # forecast
 source(paste0(dir_script_ed, 'analysis5.r')) # plot
+
+# TODO: figure out a way to store params for models
