41ebc8503e6899cd99d8901858e1032a3128ccd9
eunices <eunice.sjy@gmail.com>
2019-10-12 10:08:53 +0800

    [EJYS] Remove plots

--- 2019-07-15-edie-et-al/analysis.r
+++ 2019-07-15-edie-et-al/analysis.r
@@ -17,7 +17,7 @@ if (!exists("model_params")) {
         dataset = "LT", # BG = biogeographic realms,  GL = global, BM = biomes, LT = latitude-trop/not
         ll = "N",       # whether using lat lon data (Y) or global.distribution data (N)
         chains = 4,     # stan's number of chains
-        iter = 50000,  # stan's number of iterations
+        iter = 100000,  # stan's number of iterations
         ad = 0.999,      # stan's adapt_delta
         td = 12         # stan's max tree depth
     )   # note: GL and BM always Y; the rest can be either Y or N
