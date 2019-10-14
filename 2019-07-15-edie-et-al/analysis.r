084f6260e555e79efc140d631b3be2bfa12da0b7
eunices <eunice.sjy@gmail.com>
2019-10-14 21:43:48 +0800

    [EJYS] Reformat the calling scripts for analysis pipeline, realize parameters are not passed

--- 2019-07-15-edie-et-al/analysis.r
+++ 2019-07-15-edie-et-al/analysis.r
@@ -7,40 +7,64 @@ print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 # source(paste0(dir_script_ed, "analysis.r"))
 
 source('2019-07-15-edie-et-al/init_a.r')
+source('2019-07-15-edie-et-al/analysis_loops_params.r')
 
 # Model parameters
 #############
-if (!exists("model_params")) {
+analysis_edie_loops <- "Y"
+
+if (analysis_edie_loops == "N") {
     model_params <- list(
         dataset = "BM", # BG = biogeographic realms,  GL = global, BM = biomes, LT = latitude-trop/not
         ll = "Y",       # whether using lat lon data (Y) or global.distribution data (N)
         chains = 4,     # stan's number of chains
-        iter = 5000,  # stan's number of iterations
+        iter = 5000,    # stan's number of iterations
         ad = 0.99,      # stan's adapt_delta
         td = 15         # stan's max tree depth
     )   # note: GL and BM always Y; the rest can be either Y or N
-}
 
-# Scripts
-#############
-# Initialize identifier
-model_identifier <- paste0(
-    model_params$dataset, model_params$ll, "-",
-    "C", as.character(model_params$chains), "-",
-    "I", as.character(model_params$iter), "-",
-    "A", as.character(model_params$ad), "-",
-    "T", as.character(model_params$td))
-dir_model_folder <- paste0(dir_analysis_edie_tmp, "/", model_identifier, "/")
-dir.create(dir_model_folder); dir.create(file.path(dir_model_folder, 'output'))
-filepath_log <- paste0(dir_model_folder, "/model.log"); if (!file.exists(filepath_log)) file.create(filepath_log)
+} else if (analysis_edie_loops == "Y") {
+    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+    speed_len <- length(chosen_speeds); param_len <- length(chosen_indices)
+    print(paste0(Sys.time(), " --- Start modelling loop for ", speed_len, " x ", 
+                 param_len, " = ", speed_len*param_len, " combinations."))
+    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
 
-# Analysis scripts
-source(paste0(dir_script_ed, 'analysis0.r')) # data prep
-source(paste0(dir_script_ed, 'analysis1.r')) # data prep
-source(paste0(dir_script_ed, 'analysis2.r')) # model fitting
-source(paste0(dir_script_ed, 'analysis3.r')) # post
-source(paste0(dir_script_ed, 'analysis4.r')) # forecast
-source(paste0(dir_script_ed, 'analysis5.r')) # plot
+    
+    for (j in 1:speed_len) {
+        for (i in 1:param_len) {
+            chosen_index <- chosen_indices[i]; chosen_speed <- chosen_speeds[j]
+            print("#############################################################")
+            print("#############################################################")
+            print("#############################################################")
+            print(paste0(Sys.time(), " --- Modelling for chosen_index = ", chosen_index, 
+                        "; speed = ", chosen_speed))
+            model_params <- model_params_combinations(chosen_speed)[[chosen_index]]
 
-# remove all variables after each run (but not functions)
-rm(list = setdiff(ls(), lsf.str()))
+            # Initialize identifier
+            model_identifier <- paste0(
+            model_params$dataset, model_params$ll, "-",
+            "C", as.character(model_params$chains), "-",
+            "I", as.character(model_params$iter), "-",
+            "A", as.character(model_params$ad), "-",
+            "T", as.character(model_params$td))
+            dir_model_folder <- paste0(dir_analysis_edie_tmp, "/", model_identifier, "/")
+            dir.create(dir_model_folder); dir.create(file.path(dir_model_folder, 'output'))
+            filepath_log <- paste0(dir_model_folder, "/model.log"); if (!file.exists(filepath_log)) file.create(filepath_log)
+
+            tryCatch({
+                # print("Model params:"); print(model_params)
+                # Analysis scripts
+                source(paste0(dir_script_ed, 'analysis0.r')) # data prep
+                source(paste0(dir_script_ed, 'analysis1.r')) # data prep
+                source(paste0(dir_script_ed, 'analysis2.r')) # model fitting
+                source(paste0(dir_script_ed, 'analysis3.r')) # post
+                source(paste0(dir_script_ed, 'analysis4.r')) # forecast
+                source(paste0(dir_script_ed, 'analysis5.r')) # plot
+
+            }, error=function(e) {print(paste0("ERROR: ", conditionMessage(e)))})
+        }
+    }
+} 
--- 2019-07-15-edie-et-al/analysis_loops_params.r
+++ 2019-07-15-edie-et-al/analysis_loops_params.r
@@ -1,7 +1,3 @@
-# Set up
-#############
-source('2019-07-15-edie-et-al/init_a.r') # init
-
 # Parameters
 #############
 # chosen_speeds <- c('fast', 'slow1', 'slow3')
@@ -70,32 +66,5 @@ model_params_combinations <- function(chosen_speed) {
     )
 } 
 
-run_edie_analysis_loop <- function() {
 
-    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-    speed_len <- length(chosen_speeds); param_len <- length(chosen_indices)
-    print(paste0(Sys.time(), " --- Start modelling loop for ", speed_len, " x ", 
-                 param_len, " = ", speed_len*param_len, " combinations."))
-    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
 
-    for (j in 1:speed_len) {
-        for (i in 1:param_len) {
-            chosen_index <- chosen_indices[i]; chosen_speed <- chosen_speeds[j]
-            print("#############################################################")
-            print("#############################################################")
-            print("#############################################################")
-            print(paste0(Sys.time(), " --- Modelling for chosen_index = ", chosen_index, 
-                        "; speed = ", chosen_speed))
-            model_params <- model_params_combinations(chosen_speed)[[chosen_index]]
-            # print(model_params)
-            tryCatch({
-                source(paste0(dir_script_ed, 'analysis.r'))
-            }, error=function(e) {print(paste0("ERROR: ", conditionMessage(e)))})
-        }
-    }
-
-}
-
-run_edie_analysis_loop()
