b7f4e4f646ce2ea2b2e5f64469dff71817643f1d
eunices <eunice.sjy@gmail.com>
2019-10-14 19:08:28 +0800

    [EJYS] Fix minor erros to pipeline

--- 2019-07-15-edie-et-al/analysis_loops.r
+++ 2019-07-15-edie-et-al/analysis_loops.r
@@ -1,12 +1,17 @@
-# Init script
-source('2019-07-15-edie-et-al/init_a.r')
+# Set up
+#############
+source('2019-07-15-edie-et-al/init_a.r') # init
+options(scipen = 999)
 
 # Parameters
-chosen_speeds <- c('fast', 'slow1', 'slow3')
-chosen_indices <- c(2, 3)
+#############
+# chosen_speeds <- c('fast', 'slow1')
+# chosen_indices <- c(3)
+chosen_speeds <- c('fast') 
+chosen_indices <- c(6, 5, 4, 3, 2, 1) # 6 options
 
 # Settings (predefined)
-speeds <- list(slow1 = list(iterations =  100000, 
+speeds <- list(slow1 = list(iterations = 100000,
                            adapt_delta = 0.999,
                            tree_depth = 15,
                            chains = 4),
@@ -18,64 +23,92 @@ speeds <- list(slow1 = list(iterations =  100000,
                            adapt_delta = 0.999,
                            tree_depth = 15,
                            chains = 4),
-               fast = list(iterations =  50000, 
+               fast = list(iterations =  10000, 
                            adapt_delta = 0.99,
                            tree_depth = 12,
                            chains = 4))
 
-model_params_combinations <- list(
-   list(dataset = "GL", 
-        ll = "Y",       
-        chains = speeds[[chosen_speed]]$chains,     
-        iter = speeds[[chosen_speed]]$iterations,  
-        ad = speeds[[chosen_speed]]$adapt_delta,      
-        td = speeds[[chosen_speed]]$tree_depth),
-   list(dataset = "BG", 
-        ll = "Y",       
-        chains = speeds[[chosen_speed]]$chains,     
-        iter = speeds[[chosen_speed]]$iterations,  
-        ad = speeds[[chosen_speed]]$adapt_delta,      
-        td = speeds[[chosen_speed]]$tree_depth),
-   list(dataset = "BG", 
-        ll = "N",       
-        chains = speeds[[chosen_speed]]$chains,     
-        iter = speeds[[chosen_speed]]$iterations,  
-        ad = speeds[[chosen_speed]]$adapt_delta,      
-        td = speeds[[chosen_speed]]$tree_depth),
-   list(dataset = "LT", 
-        ll = "Y",       
-        chains = speeds[[chosen_speed]]$chains,     
-        iter = speeds[[chosen_speed]]$iterations,  
-        ad = speeds[[chosen_speed]]$adapt_delta,      
-        td = speeds[[chosen_speed]]$tree_depth),
-   list(dataset = "LT", 
-        ll = "N",       
-        chains = speeds[[chosen_speed]]$chains,     
-        iter = speeds[[chosen_speed]]$iterations,  
-        ad = speeds[[chosen_speed]]$adapt_delta,      
-        td = speeds[[chosen_speed]]$tree_depth),
-   list(dataset = "BM", 
-        ll = "Y",       
-        chains = speeds[[chosen_speed]]$chains,     
-        iter = speeds[[chosen_speed]]$iterations,  
-        ad = speeds[[chosen_speed]]$adapt_delta,      
-        td = speeds[[chosen_speed]]$tree_depth)
-)
+model_params_combinations <- function(chosen_speed) {
+    list(
+        list(dataset = "GL", 
+                ll = "Y",       
+                chains = speeds[[chosen_speed]]$chains,     
+                iter = speeds[[chosen_speed]]$iterations,  
+                ad = speeds[[chosen_speed]]$adapt_delta,      
+                td = speeds[[chosen_speed]]$tree_depth),
+        list(dataset = "BG", 
+                ll = "Y",       
+                chains = speeds[[chosen_speed]]$chains,     
+                iter = speeds[[chosen_speed]]$iterations,  
+                ad = speeds[[chosen_speed]]$adapt_delta,      
+                td = speeds[[chosen_speed]]$tree_depth),
+        list(dataset = "BG", 
+                ll = "N",       
+                chains = speeds[[chosen_speed]]$chains,     
+                iter = speeds[[chosen_speed]]$iterations,  
+                ad = speeds[[chosen_speed]]$adapt_delta,      
+                td = speeds[[chosen_speed]]$tree_depth),
+        list(dataset = "LT", 
+                ll = "Y",       
+                chains = speeds[[chosen_speed]]$chains,     
+                iter = speeds[[chosen_speed]]$iterations,  
+                ad = speeds[[chosen_speed]]$adapt_delta,      
+                td = speeds[[chosen_speed]]$tree_depth),
+        list(dataset = "LT", 
+                ll = "N",       
+                chains = speeds[[chosen_speed]]$chains,     
+                iter = speeds[[chosen_speed]]$iterations,  
+                ad = speeds[[chosen_speed]]$adapt_delta,      
+                td = speeds[[chosen_speed]]$tree_depth),
+        list(dataset = "BM", 
+                ll = "Y",       
+                chains = speeds[[chosen_speed]]$chains,     
+                iter = speeds[[chosen_speed]]$iterations,  
+                ad = speeds[[chosen_speed]]$adapt_delta,      
+                td = speeds[[chosen_speed]]$tree_depth)
+    )
+} 
 
-if (length(chosen_indices) == 1 & length(chosen_speeds) == 1) {
-    chosen_index <- chosen_indices[1]
-    chosen_speed <- chosen_speeds[1]
-    print(paste0("Modelling for chosen_index = ", chosen_index))
-    model_params <- model_params_combinations[[chosen_index]]
-    source(paste0(dir_script_ed, 'analysis.r'))
-} else {
-    for (j in 1:length(chosen_speeds)) {
-        for (i in 1:length(chosen_indices)) {
-            chosen_index <- chosen_indices[i]; chosen_speed <- chosen_speeds[j]
-            print(paste0("Modelling for chosen_index = ", chosen_index))
-            model_params <- model_params_combinations[[chosen_index]]
-            source(paste0(dir_script_ed, 'analysis.r'))
+run_edie_analysis_loop <- function() {
+
+    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+    speed_len <- length(chosen_speeds); param_len <- length(chosen_indices)
+    print(paste0(Sys.time(), " --- Start modelling loop for ", speed_len, " x ", 
+                 param_len, " = ", speed_len*param_len, " combinations."))
+    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+
+    if (param_len == 1 &  speed_len == 1) {
+
+        chosen_index <- chosen_indices[1]
+        chosen_speed <- chosen_speeds[1]
+        print("#############################################################")
+        print("#############################################################")
+        print("#############################################################")
+        print(paste0(Sys.time(), " --- Modelling for chosen_index = ", chosen_index, 
+                    "; speed = ", chosen_speed))
+        model_params <- model_params_combinations(chosen_speed)[[chosen_index]]
+        # print(model_params)
+        source(paste0(dir_script_ed, 'analysis.r'))
+
+    } else {
+
+        for (j in 1:speed_len) {
+            for (i in 1:param_len) {
+                chosen_index <- chosen_indices[i]; chosen_speed <- chosen_speeds[j]
+                print("#############################################################")
+                print("#############################################################")
+                print("#############################################################")
+                print(paste0(Sys.time(), " --- Modelling for chosen_index = ", chosen_index, 
+                            "; speed = ", chosen_speed))
+                model_params <- model_params_combinations(chosen_speed)[[chosen_index]]
+                # print(model_params)
+                source(paste0(dir_script_ed, 'analysis.r'))
+            }
         }
-    }
 
+    }
 }
+
+run_edie_analysis_loop()
