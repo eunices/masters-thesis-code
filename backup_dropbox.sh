dcabb6b3410eaefd307b7190f6a816f2fa745717
Eunice Soh <eunice.sjy@gmail.com>
2019-10-23 17:19:10 +0800

    [EJYS] Make naming more explicit

--- backup_dropbox.sh
+++ backup_dropbox.sh
@@ -8,6 +8,6 @@ echo Today is "$d"
 file=msc-thesis-code-data-$d
 
 # Option 1: as zip file in dropbox
-zip -r $file data/lookup/* data/geo_processed/*
+zip -r $file data/lookup/*
 dir="../../Dropbox/msc-thesis/data-dump/backups"
 mv $file.zip $dir
