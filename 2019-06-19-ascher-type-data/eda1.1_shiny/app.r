9d4bd4e27a55643eaeecedffbf144df1c446312c
Eunice Soh <eunice.sjy@gmail.com>
2019-10-24 20:49:09 +0800

    [EJYS] Improve UI

--- 2019-06-19-ascher-type-data/eda1.1_shiny/app.r
+++ 2019-06-19-ascher-type-data/eda1.1_shiny/app.r
@@ -113,8 +113,8 @@ server <- function(input, output) {
         NodeID = "label", Group="label", Nodesize="N", arrow=T, zoom=T, opacity=.8, 
         radiusCalculation = JS("Math.sqrt(d.nodesize)"),
         linkWidth = JS("function(d) { return d.value == 1 ? 1 : Math.log(d.value, 2); }"),
-        charge=-20000/(input$thresholdInput+1), fontFamily="Calibri", 
-        fontSize=20, opacityNoHover = 1, clickAction=clickJS)
+        charge=-10000/((input$thresholdInput/100+1)), fontFamily="Calibri", 
+        fontSize=40, opacityNoHover = 1, clickAction=clickJS)
     }
   })
 
