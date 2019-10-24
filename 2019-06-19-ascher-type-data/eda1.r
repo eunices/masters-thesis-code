3c81d0d9fb35995aeb8fc56963b902d28e13c018
Eunice Soh <eunice.sjy@gmail.com>
2019-10-24 20:37:30 +0800

    [EJYS] Add section on shiny app

--- 2019-06-19-ascher-type-data/eda1.r
+++ 2019-06-19-ascher-type-data/eda1.r
@@ -59,11 +59,14 @@ ss <- merge(ss, lookup.cty[, c("GEC", "Country", "A.3")],
 write.csv(ss[order(-prop)],
           paste0(dir_data, "eda1_flow/2019-09-22-summary-country-prop.csv"), na='', row.names=F, fileEncoding="UTF-8")
 
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# Section - Launch shiny apps
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 # shiny::runApp('2019-06-19-ascher-type-data/eda1.1_shiny')
-rsconnect::setAccountInfo(name='healthspan',
-			              token=shiny_token,
-			              secret=shiny_secret)
-rsconnect::deployApp('2019-06-19-ascher-type-data/eda1.1_shiny')
+# rsconnect::setAccountInfo(name='ejysoh',
+# 			              token=shiny_token,
+# 			              secret=shiny_secret)
+# rsconnect::deployApp('2019-06-19-ascher-type-data/eda1.1_shiny')
 
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 # Section - Location analysis suggested by Ascher
