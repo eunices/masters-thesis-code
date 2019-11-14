155b52ff4f7f244cead112ef517181c20f7fd913
Eunice Soh <eunice.sjy@gmail.com>
2019-11-14 15:11:35 +0800

    [EJYS] Plotting equality curve

--- 2019-06-19-ascher-type-data/eda3.2.r
+++ 2019-06-19-ascher-type-data/eda3.2.r
@@ -4,18 +4,23 @@
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 print(paste0(Sys.time(), " --- load data"))
 
+# Read/initialize all data
+
+# Plausible UN data
 un_path <- "data/2019-11-11-un-indicators/"
 df_r <- fread(paste0(un_path, '2019-11-12-indicators.csv'), encoding="UTF-8", stringsAsFactors=F, na=c(""))
 names_df_r <- df_r[1,]; df_r <- df_r[-1,]
 names(df_r) <- unlist(names_df_r, use.names=FALSE)
 
-filepath <- '2019-05-23-Apoidea world consensus file Sorted by name 2019 describers_4.0-denormalised2.csv'
-dat <- fread(paste0(dir_data, filepath), encoding="UTF-8", stringsAsFactors=F, na=c(""))
-cols <- c('idxes', 'full.name.of.describer.n', 'idxes_author.order', 'date.n')
-dat <- dat[,..cols]
-
+# Lookup table for countries
 lu <- fread('data/lookup/2019-05-29-statoid-country-codes.csv',  encoding="UTF-8")
 
+# Denormalised authors
+filepath <- '2019-05-23-Apoidea world consensus file Sorted by name 2019 describers_4.0-denormalised2.csv'
+dat <- fread(paste0(dir_data, filepath), encoding="UTF-8", stringsAsFactors=F, na=c(""))
+cols <- c('idxes', 'full.name.of.describer.n', 'idxes_author.order', 'date.n'); dat <- dat[,..cols]
+
+# Author info
 auth <- get_des(write=F)
 auth <- auth[, c('full.name.of.describer.n', 'residence.country.describer.n', 'describer.gender.n')]
 auth$residence.country.describer.n <- sapply(auth$residence.country.describer.n, function(x) strsplit(x, "; ")[[1]][1])
@@ -23,21 +28,40 @@ auth <- merge(auth, lu[, c("DL", "Country")], all.x=T, all.y=F,
               by.x="residence.country.describer.n", by.y="DL")
 auth$residence.country.describer.n <- NULL
 
-dat <- merge(dat, auth[, c("full.name.of.describer.n", "describer.gender.n")],
+# Merge dataframes
+dat <- merge(dat, auth[, c("full.name.of.describer.n", "describer.gender.n", "Country")],
              all.x=T, all.y=F, by="full.name.of.describer.n")
-prop <- dat[, .N,by=c("date.n", "describer.gender.n")]
-prop <- dcast(prop, date.n ~ describer.gender.n, value.var="N")
-prop$date <- prop$date.n - min(prop$date.n)
-prop <- merge(prop, data.frame(date=0:max(prop$date)),
-              all.x=T, all.y=T, by="date")
-prop[is.na(prop)] <- 0
-prop$prop_F <- prop$F / (prop$M + prop$F)
-prop$N <- prop$F + prop$M
 
-first_year_female <- min(prop[F>0]$date)
-prop <- prop[date>first_year_female]
-prop$date <- prop$date - (first_year_female + 1)
+generate_prop_t <- function(country="All") { 
+    # Filtering for each country should happen here
+    print(head(dat))
+    if (country != "All") {
+        dat <- dat[Country == country]
+    }
 
-names(prop)[which(names(prop) == 'F')] <- "nFemales"
-names(prop)[which(names(prop) == 'M')] <- "nMales"
-names(prop)[which(names(prop) == 'N')] <- "n"
+    # count N by gender
+    prop <- dat[, .N,by=c("date.n", "describer.gender.n")]
+    prop <- dcast(prop, date.n ~ describer.gender.n, value.var="N")
+
+    # normalize date to minimum year with >0
+    prop$date <- prop$date.n - min(prop$date.n)
+    prop <- merge(prop, data.frame(date=0:max(prop$date)),
+                all.x=T, all.y=T, by="date")
+    prop[is.na(prop)] <- 0
+
+    # tabulate proportion
+    prop$prop_F <- prop$F / (prop$M + prop$F)
+    prop$N <- prop$F + prop$M
+
+    # filter data to year with at least 1 female
+    first_year_female <- min(prop[F>0]$date)
+    prop <- prop[date>first_year_female]
+    prop$date <- prop$date - (first_year_female + 1)
+
+    # rename data
+    names(prop)[which(names(prop) == 'F')] <- "nFemales"
+    names(prop)[which(names(prop) == 'M')] <- "nMales"
+    names(prop)[which(names(prop) == 'N')] <- "n"
+
+    return prop
+}
--- 2019-06-19-ascher-type-data/eda3.r
+++ 2019-06-19-ascher-type-data/eda3.r
@@ -1,5 +1,7 @@
 source('2019-06-19-ascher-type-data/subset.r')
 
+
+# Libraries
 library(dplyr)
 library(RSQLite)
 
@@ -7,11 +9,13 @@ library(tidyverse)
 library(httr)
 library(jsonlite)
 
-theme <- theme_classic()
+# Scripts
+source('2019-06-19-ascher-type-data/eda3_util.r') # util functions
+# source('2019-06-19-ascher-type-data/eda3.1.r') # get data from UN 's API and save locally
+source('2019-06-19-ascher-type-data/eda3.2.r') # read local/ bee data
 
-source('2019-06-19-ascher-type-data/eda3_util.r')
-# source('2019-06-19-ascher-type-data/eda3.1.r')
-source('2019-06-19-ascher-type-data/eda3.2.r')
+# Parameters
+theme <- theme_classic()
 
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 # Section - gender rep analysis
@@ -20,14 +24,34 @@ print(paste0(Sys.time(), " --- gender rep analysis"))
 
 # adapted from https://github.com/lukeholman/genderGapCode/
 
-# as a whole
+##################################
+# overall
+##################################
 
+# for plotting purposes
+prop <- generate_prop_t(country="All")
 res <- find.response.variables(prop)
-df_p <- data.frame(x=1:200,
-                   y=sapply(1:200, pfunc, r=res$r, c=res$c))
-df_p <- merge(df_p, prop[, c('date', 'prop_F')], by.x="x", by.y="date", all.x=T, all.y=T)
+max_predict_year <- as.numeric(paste0(as.numeric(substr(as.character(max(prop$date.n)), 1, 2)) + 1, "00"))
+df_p <- data.frame(x= 0:max_predict_year+ min(prop$date.n),
+                   y=sapply(0:max_predict_year, pfunc, r=res$r, c=res$c))
+df_p <- merge(df_p, prop[, c('date.n', 'prop_F')], by.x="x", by.y="date.n", all.x=T, all.y=T)
+
+df_p$y <- round(df_p$y*100, 5)
+df_p$prop_F <- round(df_p$prop_F*100, 5)
 
 ggplot(df_p) + 
-  geom_bar(stat="identity",  aes(x=x, y=y)) + 
+  geom_bar(stat="identity",  aes(x=x, y=y), fill="grey") + 
   geom_bar(stat="identity", aes(x=x, y=prop_F), fill = "#FF6666") + 
-  theme + xlab("Year") + ylab("Proportion of authors (%)")
+  geom_vline(xintercept= (res$parity.year + min(prop$date.n)), linetype="dashed", size=1.5, color="black") +
+  theme + xlab("Year") + ylab("Proportion of female-authored species, overall (%)") + ylim(c(0,50))
+
+# get CI
+
+
+##################################
+# usa, germany, brazil, france, united kingdom, japan [top 6 countries] 
+# as case studies; they have more than 30 taxonomists across the years
+# and potentially have interesting stories to tell
+##################################
+countries <- c("United States of America", "Germany", "Brazil", "France", "United Kingdom", "Japan")
+auth[, .N, by=Country][order(-N)][1:6]
