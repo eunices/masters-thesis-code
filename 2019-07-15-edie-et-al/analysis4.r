7f74ea58f58a2038c02df42b517684a5382b3686
Eunice Soh <eunice.sjy@gmail.com>
2019-12-05 15:43:41 +0800

    [EJYS] Improve docstrings

--- 2019-07-15-edie-et-al/analysis4.r
+++ 2019-07-15-edie-et-al/analysis4.r
@@ -16,64 +16,70 @@ rm(fit) # remove from memory
 
 post.forecast <- function(data, ftime, model) {
 
-    #' 
-    #' 
-    #' 
-    #'  
+  #' Make predictions for time series
+  #' 
+  #' Given a model, make predictions based on a predefined timestep
+  #' @param data Data as output by initial data processing step i.e. count_info.data.R
+  #' @param ftime Number of years to predict 
+  #' @param model rStan model i.e. fit.data
 
-    # sample from model posterior
-    mp <- sample_model_posterior_parameters(model)
-    coef0=mp$coef0; coef1=mp$coef1; alp=mp$alp; bet=mp$bet;
-    gam=mp$gam; eta=mp$eta; phi=phi; rm(mp)
+  # sample from model posterior
+  mp <- sample_model_posterior_parameters(model)
+  coef0=mp$coef0; coef1=mp$coef1; alp=mp$alp; bet=mp$bet;
+  gam=mp$gam; eta=mp$eta; phi=phi; rm(mp)
 
-    # number of groups
-    p <- data$P 
-    # initial count
-    initial <- sapply(seq(p), function(pp) data$counts[ pp, data$str[pp]])
+  # number of groups
+  p <- data$P 
 
-    # by group
-    out <- list() # store predictions
-    for(ii in seq(p)) {
-        start <- data$str[ii]; end <- data$end[ii]
+  # initial count
+  initial <- sapply(seq(p), function(pp) data$counts[ pp, data$str[pp]])
 
-        # offset starts at first naming year
-        all_toff <- data$off[ii, ][start:end] 
+  # by group
+  out <- list() # store predictions
+  for(ii in seq(p)) {
+      start <- data$str[ii]; end <- data$end[ii]
 
-        # generate offset segment by sampling past decade of offsets
-        toff <- sample(all_toff[(length(all_toff) - ftime) : length(all_toff)], 
-                       ftime, replace=TRUE)
+      # offset starts at first naming year
+      all_toff <- data$off[ii, ][start:end]; len <- length(all_toff)
 
-        lambda <- c()
-        theta <- c()
-        oo <- data$counts[ii, end]
-        co <- c() # empty expected count vec
+      # generate offset segment by sampling past decade of offsets
+      toff <- sample(all_toff[(len-ftime):len], ftime, replace=TRUE)
 
-        # by time point
-        for(jj in 1:ftime) {  # time seq extends the series by ftime years
-          if(jj == 1) { # the starting point is the number of species described at end of time series
-            lambda[jj] <- oo
-            theta[jj] <- 0
-            co[jj] <- oo[jj]
-          } else { # if not the starting point
-            lambda[jj] <- exp(coef0[ii] + coef1[ii] * jj) +
-            alp[ii] * oo[jj - 1] + bet[ii] * lambda[jj - 1]
+      # initialize parameters
+      lambda <- c()
+      theta <- c()
+      oo <- data$counts[ii, end] # final year's count
+      co <- c() # empty expected count vec
 
-            val <- (oo[jj - 1] == 0) * 1
-            theta[jj] <- (val * gam[ii]) + ((1 - val) * eta[ii])
+      # by time point
+      
+      # set first time point
+      lambda[1] <- oo
+      theta[1] <- 0
+      co[1] <- oo[jj]
 
-            co[jj] <- rZIP(1, lambda = (toff[jj] + 1) * lambda[jj], 
-                           sigma = theta[jj])
-            oo[jj] <- co[jj] # current count becomes next expected count
-          }
-        }
-        out[[ii]] <- co
-    }
-    out
+      # set subsequent time points
+      for(jj in 2:ftime) {
+        lambda[jj] <- exp(coef0[ii] + coef1[ii] * jj) +
+          alp[ii] * oo[jj - 1] + bet[ii] * lambda[jj - 1]
+
+        z <- ifelse(oo[jj - 1] == 0, 1, 0)
+        theta[jj] <- (z * gam[ii]) + ((1 - z) * eta[ii])
+
+        co[jj] <- rZIP(1, lambda = (toff[jj] + 1) * lambda[jj], 
+                       sigma = theta[jj])
+
+        oo[jj] <- co[jj] # current count becomes next expected count
+      }
+      out[[ii]] <- co
+  }
+  out
 }
 
 # simulate the forecast
 forecast <- mclapply(1:1000, mc.cores=1, function(ii) {
    post.forecast(data=data, ftime=ftime, model=zips) 
 })
-save(forecast, file=paste0(dir_model_folder, "forecast.data")); rm(data, zips, forecast)
 
+save(forecast, file=paste0(dir_model_folder, "forecast.data"))
+rm(data, zips, forecast)
