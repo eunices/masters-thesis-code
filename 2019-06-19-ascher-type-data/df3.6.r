2db3e1b222e90c80176464464f76bb032a4d8cc4
Eunice Soh <eunice.sjy@gmail.com>
2019-12-21 18:59:56 +0800

    [EJYS] Count number of authors with <threshold species

--- 2019-06-19-ascher-type-data/df3.6.r
+++ 2019-06-19-ascher-type-data/df3.6.r
@@ -65,10 +65,53 @@ taxonomic_effort2 <-
                by=years][order(as.numeric(years))][
                    , c("years", "N_real_describers", "N_weighted_real_describers")]
 
+
+###########################
+# For taxonomic_effort3
+###########################
+N_species <- 10
+describers <- describer_data[!(idx_auth %in% to_exclude) & as.numeric(spp_N) <=N_species, 
+                             c("idx_auth", "full.name.of.describer.n", 
+                               "min", "max_corrected", "ns_species_per_year_active",
+                               "spp_N")]
+describers <- dcast(describers, 
+      idx_auth + full.name.of.describer.n +  min + 
+        max_corrected ~ as.numeric(spp_N), value.var="ns_species_per_year_active")
+
+# Figure out how to loop this # TODO:
+describers[!is.na(`2`), ]$`3` <- describers[!is.na(`2`), ]$`2`
+describers[!is.na(`3`), ]$`4` <- describers[!is.na(`3`), ]$`3`
+describers[!is.na(`4`), ]$`5` <- describers[!is.na(`4`), ]$`4`
+describers[!is.na(`5`), ]$`6` <- describers[!is.na(`5`), ]$`5`
+describers[!is.na(`6`), ]$`7` <- describers[!is.na(`6`), ]$`6`
+describers[!is.na(`7`), ]$`8` <- describers[!is.na(`7`), ]$`7`
+describers[!is.na(`8`), ]$`9` <- describers[!is.na(`8`), ]$`8`
+describers[!is.na(`9`), ]$`10` <- describers[!is.na(`9`), ]$`9`
+
+seq <- mapply(function(a, b) {
+    seq(a, b)
+}, a=describers$min, b=describers$max_corrected)
+describers$years <- seq
+describers <- data.table(describers %>% unnest(years))
+taxonomic_effort3 <- 
+    describers[, list(N_real_describers.1 = length(which(!is.na(`1`))), 
+                      N_real_describers.2 = length(which(!is.na(`2`))), 
+                      N_real_describers.3 = length(which(!is.na(`3`))), 
+                      N_real_describers.4 = length(which(!is.na(`4`))), 
+                      N_real_describers.5 = length(which(!is.na(`5`))), 
+                      N_real_describers.6 = length(which(!is.na(`6`))), 
+                      N_real_describers.7 = length(which(!is.na(`7`))), 
+                      N_real_describers.8 = length(which(!is.na(`8`))), 
+                      N_real_describers.9 = length(which(!is.na(`9`))), 
+                      N_real_describers.10 = length(which(!is.na(`10`)))
+    ), by=years][order(as.numeric(years))]
+
+
 ############################
-# Merge taxonomic_effort1&2
+# Merge taxonomic_effort
 ###########################
 taxonomic_effort <- merge(taxonomic_effort1, taxonomic_effort2, by="years", all.x=T, all.y=F)
+taxonomic_effort <- merge(taxonomic_effort, taxonomic_effort3, by="years", all.x=T, all.y=F)
 
 # TODO: plot time series of N=1 species described, N=2 etc.
 
