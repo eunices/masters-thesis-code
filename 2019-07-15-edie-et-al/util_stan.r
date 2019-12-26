227d27caf3649dedd37c25baca25f5fe3dd4d96f
Eunice Soh <eunice.sjy@gmail.com>
2019-12-26 22:30:28 +0800

    [EJYS] Modify utils for stan

--- 2019-07-15-edie-et-al/util_stan.r
+++ 2019-07-15-edie-et-al/util_stan.r
@@ -3,32 +3,38 @@
 
 # Check transitions that ended with a divergence
 check_div <- function(fit) {
+
+  print("#### CHECKING DIVERGENCES: ")
   sampler_params <- get_sampler_params(fit, inc_warmup=FALSE)
   divergent <- do.call(rbind, sampler_params)[,'divergent__']
   n = sum(divergent)
   N = length(divergent)
 
   print(sprintf('%s of %s iterations ended with a divergence (%s%%)',
-                n, N, 100 * n / N))
+                n, N, round(100 * n / N, 2)))
   if (n > 0)
-    print('  Try running with larger adapt_delta to remove the divergences')
+    print('Try running with larger adapt_delta to remove the divergences')
 }
 
 # Check transitions that ended prematurely due to maximum tree depth limit
 check_treedepth <- function(fit, max_depth = 10) {
+
+  print("#### CHECKING TREE DEPTH: ")
   sampler_params <- get_sampler_params(fit, inc_warmup=FALSE)
   treedepths <- do.call(rbind, sampler_params)[,'treedepth__']
   n = length(treedepths[sapply(treedepths, function(x) x == max_depth)])
   N = length(treedepths)
 
   print(sprintf('%s of %s iterations saturated the maximum tree depth of %s (%s%%)',
-                n, N, max_depth, 100 * n / N))
+                n, N, max_depth, round(100 * n / N, 2)))
   if (n > 0)
-    print('  Run again with max_depth set to a larger value to avoid saturation')
+    print('Run again with max_depth set to a larger value to avoid saturation')
 }
 
 # Checks the energy Bayesian fraction of missing information (E-BFMI)
 check_energy <- function(fit) {
+
+  print("#### CHECKING E-BFMI: ")
   sampler_params <- get_sampler_params(fit, inc_warmup=FALSE)
   no_warning <- TRUE
   for (n in 1:length(sampler_params)) {
@@ -36,18 +42,20 @@ check_energy <- function(fit) {
     numer = sum(diff(energies)**2) / length(energies)
     denom = var(energies)
     if (numer / denom < 0.2) {
-      print(sprintf('Chain %s: E-BFMI = %s', n, numer / denom))
+      print(sprintf('Chain %s: E-BFMI = %s', n, round(numer / denom * 100, 2)))
       no_warning <- FALSE
     }
   }
   if (no_warning)
     print('E-BFMI indicated no pathological behavior')
   else
-    print('  E-BFMI below 0.2 indicates you may need to reparameterize your model')
+    print('E-BFMI below 0.2 indicates you may need to reparameterize your model')
 }
 
 # Checks the effective sample size per iteration
 check_n_eff <- function(fit) {
+
+  print("#### CHECKING N EFFECTIVE SAMPLE SIZE: ")
   fit_summary <- summary(fit, probs = c(0.5))$summary
   N <- dim(fit_summary)[[1]]
 
@@ -56,10 +64,13 @@ check_n_eff <- function(fit) {
   no_warning <- TRUE
   for (n in 1:N) {
     ratio <- fit_summary[,5][n] / iter
-    if (ratio < 0.001) {
+    if (ratio < 0.001 & !is.na(ratio)) {
       print(sprintf('n_eff / iter for parameter %s is %s!',
                     rownames(fit_summary)[n], ratio))
       no_warning <- FALSE
+    }
+    if (is.na(ratio)) {
+      print(sprintf('Note: Parameter %s is NA', rownames(fit_summary)[n]))
     }
   }
   if (no_warning)
@@ -70,16 +81,21 @@ check_n_eff <- function(fit) {
 
 # Checks the potential scale reduction factors
 check_rhat <- function(fit) {
+
+  print("#### CHECKING RHAT: ")
   fit_summary <- summary(fit, probs = c(0.5))$summary
   N <- dim(fit_summary)[[1]]
 
   no_warning <- TRUE
   for (n in 1:N) {
     rhat <- fit_summary[,6][n]
-    if (rhat > 1.1 || is.infinite(rhat) || is.nan(rhat)) {
+    if ((rhat > 1.1 || is.infinite(rhat)) & !is.na(rhat)) {
       print(sprintf('Rhat for parameter %s is %s!',
                     rownames(fit_summary)[n], rhat))
       no_warning <- FALSE
+    }
+    if (is.nan(rhat)) {
+      print(sprintf('Note: Parameter %s is NA', rownames(fit_summary)[n]))
     }
   }
   if (no_warning)
