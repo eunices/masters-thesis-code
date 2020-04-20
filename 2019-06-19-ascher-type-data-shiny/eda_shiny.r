fea4750655d9c7ddbf467e308de53ebee010c6ee
Eunice Soh <eunice.sjy@gmail.com>
2020-04-20 18:30:14 +0800

    [EJYS] Bug fix on shiny app directory

--- 2019-06-19-ascher-type-data-shiny/eda_shiny.r
+++ 2019-06-19-ascher-type-data-shiny/eda_shiny.r
@@ -12,10 +12,10 @@
 source('keys.r')
 
 # Launch apps
-shiny::runApp('2019-06-19-ascher-type-data/eda1.1_shiny')
-shiny::runApp('2019-06-19-ascher-type-data/eda2.1_shiny')
+shiny::runApp('2019-06-19-ascher-type-data-shiny/eda1.1_shiny')
+shiny::runApp('2019-06-19-ascher-type-data-shiny/eda2.1_shiny')
 
 # Deploy apps
 rsconnect::setAccountInfo(name='ejysoh', token=shiny_token, secret=shiny_secret)
-rsconnect::deployApp('2019-06-19-ascher-type-data/eda1.1_shiny', account="ejysoh")
-rsconnect::deployApp('2019-06-19-ascher-type-data/eda2.1_shiny', account="ejysoh")
+rsconnect::deployApp('2019-06-19-ascher-type-data-shiny/eda1.1_shiny', account="ejysoh")
+rsconnect::deployApp('2019-06-19-ascher-type-data-shiny/eda2.1_shiny', account="ejysoh")
