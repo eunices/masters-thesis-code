2786ceb1881147bc5ed099f3e50085a623a1c2e0
Eunice Soh <eunice.sjy@gmail.com>
2020-06-12 17:25:19 +0800

    [EJYS] Improve on the var.r

--- 2019-06-19-jsa-type/init/var.r
+++ 2019-06-19-jsa-type/init/var.r
@@ -1,7 +1,7 @@
 # Variables
 dir_data <- 'data/2019-05-23-ascher-bee-data/'
 
-version = "v1" # data version
+version = "v2" # data version
 # v1 on 2019-05-23
 
 # Output data
@@ -16,6 +16,7 @@ dir_data_ch3_gender <- paste0(dir_data_analysis, 'ch3-gender/')
 dir_data_raw <- paste0(dir_data, version, "/")
 dir_data_raw_clean <- paste0(dir_data_raw, "clean/")
 
+# Script folder
 dir_base <- '2019-06-19-jsa-type'
 dir_script <- paste0(dir_base, '/')
 dir_shiny <- paste0(dir_base, '-shiny/')
@@ -25,10 +26,21 @@ dir_ch3a <- paste0(dir_base, '-ch3-coauth/')
 dir_ch3b <- paste0(dir_base, '-ch3-flow/')
 dir_ch3c <- paste0(dir_base, '-ch3-gender/')
 
+# Base file folder
 basefile <- '2019-05-23-Apoidea world consensus file Sorted by name 2019'
+
+# Other 
 source('keys.R')
 source(paste0(dir_script, 'init/util.r'))
 source(paste0(dir_script, 'init/libraries.r'))
 
 # Initialize google api for geocoding
 register_google(key = geocode_api)
+
+# If data dir does not exist, create it
+data_dirs = c(dir_data, dir_data_analysis, dir_data_ch1,
+              dir_data_ch2, dir_data_ch3_coauth, dir_data_ch3_flow, dir_data_ch3_gender,
+              dir_data_raw, dir_data_raw_clean)
+lapply(data_dirs, function(folder) {
+  if(!dir.exists(folder)) dir.create(folder)
+})
