1c6e13c035f7a7cfa067065b2e6cda5eb95d048d
Eunice Soh <eunice.sjy@gmail.com>
2020-08-31 13:27:27 +0800

    [EJYS] Remove deleted files from v2 type data

--- 2020-08-15-jsa-type-v2/init/libraries.r
+++
@@ -1,4 +0,0 @@
-suppressMessages(library(RecordLinkage))
-suppressMessages(library(ggplot2))
-suppressMessages(library(ggmap))
-suppressMessages(library(data.table))
--- 2020-08-15-jsa-type-v2/init/util.r
+++
@@ -1,7 +0,0 @@
-closestWordMatch = function(string, stringVector){
-
-  distance = levenshteinSim(string, stringVector);
-  stringVector[distance == max(distance)][1]
-  # gets the closest
-
-}
