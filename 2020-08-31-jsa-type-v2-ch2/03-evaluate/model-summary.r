e49abf26fb95aff9107822d493937049e7c99fb1
Eunice Soh <eunice.sjy@gmail.com>
2020-11-17 12:59:32 +0800

    [EJYS] Fix bug on ch2

--- 2020-08-31-jsa-type-v2-ch2/03-evaluate/model-summary.r
+++ 2020-08-31-jsa-type-v2-ch2/03-evaluate/model-summary.r
@@ -3,7 +3,7 @@
 
 source('2020-08-31-jsa-type-v2-ch2/00-init/init-a.r')
 
-chosen_model <- "BGY-E2-C4-I8000-A0.8-T12-F25-V10"
+chosen_model <- "BGY-E0-C4-I8000-A0.8-T12-F25-V10"
 
 model_params <- parse_model_identifier(chosen_model)
 dir_model_folder <- initialize_model_params(model_params)[1]
@@ -12,36 +12,13 @@ load(paste0(dir_model_folder, "fit.data")) # as "fit"
 
 check_all_diagnostics(fit)
 
-c_dark <- c("#8F272780")
-green <- c("#00FF0080")
-
-partition <- partition_div(fit)
-div_params <- partition[[1]]
-nondiv_params <- partition[[2]]
-
-
-# TODO: write function for different params
-# to diagnose between pairs
-# par(mar = c(4, 4, 0.5, 0.5))
-# plot(nondiv_params$`theta[1]`, log(nondiv_params$tau),
-#      col=c_dark, pch=16, cex=0.8, xlab="theta[1]", ylab="log(tau)",
-#      xlim=c(-20, 50), ylim=c(-1,4))
-# points(div_params$`theta[1]`, log(div_params$tau),
-#        col=green, pch=16, cex=0.8)
-
 launch_shinystan(fit)
 
-# Edie et al
-# TODO: understand offset for TE in posterior samples
-# https://stat.ethz.ch/pipermail/r-help/2008-March/155920.html 
-# https://discourse.mc-stan.org/t/how-to-fit-a-simple-poisson-process/7913
-# TODO: time series CV
-# TODO: check divergent transitions https://betanalpha.github.io/assets/case_studies/divergences_and_bias.html
+
+# TODO: Check divergent transitions 
 # https://mc-stan.org/users/documentation/case-studies/divergences_and_bias.html
 # https://betanalpha.github.io/assets/case_studies/rstan_workflow.html
-# TODO: compare biomes
-# TODO: compare genus
 
-# reparametrize
+# TODO: Reparametrize to improve models
 # https://mc-stan.org/docs/2_18/stan-users-guide/reparameterization-section.html
 # https://betanalpha.github.io/assets/case_studies/divergences_and_bias.html
