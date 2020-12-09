768914386dbd50d7ad8c8e09857fa3db20dc8c37
Eunice Soh <eunice.sjy@gmail.com>
2020-12-09 18:31:32 +0800

    [EJYS] Update code for comparing time series

--- 2020-08-31-jsa-type-v2-ch2/03-evaluate/models-compare-ts.r
+++ 2020-08-31-jsa-type-v2-ch2/03-evaluate/models-compare-ts.r
@@ -52,78 +52,89 @@ posterior_sim(data, fit)
 posterior_forecast(data, 5, fit)
 
 
-
-
 # Predict for 5 (and repeat for 10, 15, 20, 25)
-posterior_sim(data_ref, fit)
+n_iterations <- 100
 
-# Using posterior_forecast, predict using naive method
-forecast <- mclapply(1:1000, mc.cores = 1, function(ii) {
-    posterior_forecast(data, vtime, fit)
-})
+dfs <- lapply(seq(5, model_params$va, 5), function(ptime) {
 
-# Modify from list to data.frame
-df_li <- lapply(1:length(forecast), function(i) {
-
-    x <- forecast[[i]]
-    x <- setDT(tstrsplit(as.character(x), ", ", fixed=TRUE))[]
-    names(x) <- paste0("year_", 1:dim(x)[2])
+    print(paste0("-------------- ", ptime))
     
-    x$year_1 <- gsub("c\\(", "", x$year_1)
-    x$year_5 <- gsub("\\)", "", x$year_5)
-    x[] <- lapply(x, as.numeric)
+    first_year <- max(data_raw$year) - model_params$va
 
-    x$group <- 1:7
-    x$sim <- i
-    
-    x
+    # Using posterior_sim, predict using data itself
+    predictions_sim <- mclapply(1:n_iterations, mc.cores = 1, function(ii) {
+        predictions_actual <- posterior_sim(data_ref, fit)
+        predictions_actual <- lapply(predictions_actual, function(x) {
+            x[(length(x)-model_params$va+1):(length(x)-model_params$va+ ptime)]
+        })
+        predictions_actual
+    })
+
+    # Using posterior_forecast, predict using naive method
+    predictions_forecast <- mclapply(1:n_iterations, mc.cores = 1, function(ii) {
+        posterior_forecast(data, ptime, fit)
+    })
+
+    # Coerce these lists of lists to data.table
+    sim_li <- lapply(1:n_iterations, function(i) {
+        x <- predictions_sim[[i]]
+        list_to_df(x, i)
+    })
+
+    forecast_li <- lapply(1:n_iterations, function(i) {
+        x <- predictions_forecast[[i]]
+        list_to_df(x, i)
+    })
+
+    sim_df <- rbindlist(sim_li)
+    sim_df$type <- "forecast"
+    forecast_df <- rbindlist(forecast_li)
+    forecast_df$type <- "naive forecast"
+    df <- rbind(forecast_df, sim_df)
+
+    # Wide to long
+    df_long <- melt(df, id.vars = c("group", "sim", "type"))
+    names(df_long)[which(names(df_long) == "variable")] <- "year"
+    names(df_long)[which(names(df_long) == "value")] <- "model"
+    df_long$year <- 
+        as.integer(gsub("year_", "", df_long$year)) + first_year - 1
+    df_long$group <- mapping[match(df_long$group, mapping$group),]$groupname
+
+    # Merge with actual counts
+    df_obs <- data.table(data_raw)[,
+        list(obs = .N),
+        by = c("year", "group")
+    ]
+
+    df_long <- merge(
+        df_long, df_obs,
+        by = c("year", "group")
+    )
+
+    df_long$ptime <- ptime
+
+    df_long
+
 })
-
-# Wide to long
-first_year <- max(data_raw$year) - 25 
-
-df <- rbindlist(df_li)
-df <- melt(df, id.vars = c("group", "sim"))
-names(df)[which(names(df) == "variable")] <- "year"
-
-df$year <- as.integer(gsub("year_", "", df$year)) + first_year - 1
-df$group <- mapping[match(df$group, mapping$group),]$groupname
-
-head(df)
-
-# Summarize by 80 CI
-df <- df[,
-    list(
-        mean = quantile(value, .5),
-        qt10 = quantile(value, .1),
-        qt90 = quantile(value, .9)
-    ),
-    by = c("group", "year")
-][order(group, year)]
-
-
-# Merge with actual counts
-
-model_params$fc
-
-df_obs <- data.table(data_raw)[,
-    list(obs = .N),
-    by = c("year", "group")
-]
-
-df <- merge(
-    df, df_obs,
-    by = c("year", "group")
-)
-
 
 # Calculate MAPE
+dfs <- rbindlist(dfs)
+dfs$diff <- abs(dfs$model - dfs$obs)
+dfs$perc <- dfs$diff / dfs$obs
+
+summary <- dfs[,
+    list(MAPE = mean(perc)), 
+    by = c("ptime", "type")
+]
 
 
+# Plot MAPE against ptime
+p1 <- ggplot(summary, aes(x = ptime, y = MAPE, fill = type)) +
+    geom_bar(stat = "identity", position = "dodge") + theme +
+    ylab("MAPE (%)\n") + xlab("\nPrediction duration") +
+    scale_fill_manual(
+        values = c('#999999','#E69F00'),
+        name = "Type"
+    )
 
-
-
-
-
-
-# Plot chart of MAPE against ftime
+p1
--- 2020-08-31-jsa-type-v2-ch2/03-evaluate/util.r
+++ 2020-08-31-jsa-type-v2-ch2/03-evaluate/util.r
@@ -21,3 +21,17 @@ get_loo <- function(model) {
     m_loo
 }
 
+
+list_to_df <- function(x, i) {
+    x <- setDT(tstrsplit(as.character(x), ", ", fixed=TRUE))[]
+    names(x) <- paste0("year_", 1:dim(x)[2])
+    
+    x[] <- lapply(x, function(x) {
+        x <- gsub("c\\(|\\)", "", x)
+        as.numeric(x)
+    })
+
+    x$group <- 1:dim(x)[1]
+    x$sim <- i
+    x
+}
