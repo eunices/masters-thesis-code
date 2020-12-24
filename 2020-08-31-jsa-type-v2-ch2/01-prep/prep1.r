fdc12abd2577b5f4ec973c32f8852bcecb8c8981
Eunice Soh <eunice.sjy@gmail.com>
2020-12-24 14:30:50 +0800

    [EJYS] Fix bug

--- 2020-08-31-jsa-type-v2-ch2/01-prep/prep1.r
+++ 2020-08-31-jsa-type-v2-ch2/01-prep/prep1.r
@@ -6,7 +6,7 @@ if (model_params$dataset == "BG") {        # biogeographic realm
 } else if (model_params$dataset == "BM") { # biome
     source("2020-08-31-jsa-type-v2-ch2/01-prep/prep1/bmy.r")
 } else if (model_params$dataset == "LT") { # latitude
-    # source("2020-08-31-jsa-type-v2-ch2/01-prep/prep1/lty.r")
+    source("2020-08-31-jsa-type-v2-ch2/01-prep/prep1/lty.r")
 } else if (model_params$dataset == "FA") { # family
     source("2020-08-31-jsa-type-v2-ch2/01-prep/prep1/fam.r")
 } else if (model_params$dataset == "HA") { # halictidae
--- 2020-08-31-jsa-type-v2-ch2/params-02-model.r
+++ 2020-08-31-jsa-type-v2-ch2/params-02-model.r
@@ -7,3 +7,27 @@
 # Model parameters
 model_folder_names <- c("BGY-E0-C4-I8000-A0.8-T12-F25-V0") # fast run
 
+# model_folder_names <- c(
+#     "BGY-E0-C4-I8000-A0.8-T12-F50-V50",
+#     "BGY-E1-C4-I8000-A0.8-T12-F50-V50",
+#     "BGY-E2-C4-I8000-A0.8-T12-F50-V50",
+
+#     "BGY-E0-C4-I8000-A0.8-T12-F25-V0",
+#     "BGY-E1-C4-I8000-A0.8-T12-F25-V0",
+#     "BGY-E2-C4-I8000-A0.8-T12-F25-V0",
+
+#     "BMY-E0-C4-I8000-A0.8-T12-F25-V0",
+#     "FAM-E0-C4-I8000-A0.8-T12-F25-V0",
+#     "HAL-E0-C4-I8000-A0.8-T12-F25-V0",
+#     "LTY-E0-C4-I8000-A0.8-T12-F25-V0",
+
+#     "BMY-E1-C4-I8000-A0.8-T12-F25-V0",
+#     "FAM-E1-C4-I8000-A0.8-T12-F25-V0",
+#     "HAL-E1-C4-I8000-A0.8-T12-F25-V0",
+#     "LTY-E1-C4-I8000-A0.8-T12-F25-V0",
+
+#     "BMY-E2-C4-I8000-A0.8-T12-F25-V0",
+#     "FAM-E2-C4-I8000-A0.8-T12-F25-V0",
+#     "HAL-E2-C4-I8000-A0.8-T12-F25-V0",
+#     "LTY-E2-C4-I8000-A0.8-T12-F25-V0"
+# )
