2245a76b2f00ab6a52b6004b3b78ce3c24efb2a1
Eunice Soh <eunice.sjy@gmail.com>
2021-01-27 15:08:30 +0800

    [EJYS] Update thesis

--- 2020-08-31-jsa-type-v2-ch1/01-main/in-text.r
+++ 2020-08-31-jsa-type-v2-ch1/01-main/in-text.r
@@ -16,16 +16,3 @@ dim(df_describers[spp_N_1st_auth_s>=1])
 
 # Date of birth
 dim(df_describers[spp_N_1st_auth_s>=1 & !is.na(dob.describer)])
-df_describers[spp_N_1st_auth_s>=1 & is.na(dob.describer)]$dob.describer
-
-# Results
-
-# Hyperdiverse Megachile
-meg <- df[
-    tolower(genus)=="megachile", 
-    list(N=.N), 
-    by=c("type.repository.n_short", "country.of.type.repository.n_long")
-][order(-N)]
-sum(meg$N)
-sum(meg[type.repository.n_short != "[unknown]"][1:20]$N)
-table(meg[type.repository.n_short != "[unknown]"][1:20]$country.of.type.repository.n_long)
