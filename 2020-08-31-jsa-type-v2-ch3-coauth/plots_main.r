9f1ba10c4214fd8221c2d7833dbb8f71cd705425
Eunice Soh <eunice.sjy@gmail.com>
2021-02-01 16:59:31 +0800

    [EJYS] Edit coauthor stuff

--- 2020-08-31-jsa-type-v2-ch3-coauth/plots_main.r
+++ 2020-08-31-jsa-type-v2-ch3-coauth/plots_main.r
@@ -25,23 +25,26 @@ df <- df[
   c("full.name.of.describer", "idx", "date")
 ]
 
+df$date <- as.integer(df$date)
 df <- df[date <= date_cutoff]
+df <- df[order(date)]
 
 df$date.decade <- paste0(substr(as.character(df$date), 1, 3), "0s")
 df$coauth <- "Did not coauthor"
 df[grepl("; ", full.name.of.describer)]$coauth <- "Coauthor"
 
+df_full <- df
+
+# Count number of authors
 df <- df %>% separate_rows(full.name.of.describer, sep="; ")
 df <- data.table(unique(df))
-df <- df[order(date)]
-
-df_did_not_coauth <- df[coauth == "Did not coauthor"]  # for coauth analysis
-df_coauth <- df[coauth == "Coauthor"] # for coauth analysis
-
-df <- df[, list(.N), by=c('idx', 'date.decade')]
+df <- df[, list(.N), by=c('idx', 'date.decade', 'date')]
 df <- df[order(date.decade)]
 df$N <- as.character(df$N)
 
+# Create dataset for coauth
+df_did_not_coauth <- df[N == 1]  # for coauth analysis
+df_coauth <- df[N >= 2] # for coauth analysis
 
 
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
@@ -49,50 +52,70 @@ df$N <- as.character(df$N)
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 print(paste0(Sys.time(), " --- Number of co-authoring authors"))
 dim(df)
+table(df$N)
+prop.table(table(df$N)) * 100
 prop.table(table(df$N %in% c(2,3,4)))*100
 
 
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-# Section - Mean year of publication for co-authors
+# Section - % of author who coauthored
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 nw <- get_describer_network()
+
 auth_coauth <- unique(c(nw[!is.na(p2)]$p1, nw[!is.na(p2)]$p2))
+
 auth_did_not_coauth <- unique(nw[is.na(p2)]$p1)
 
 auth_did_not_coauth <- unique(
     auth_did_not_coauth[!auth_did_not_coauth %in% auth_coauth]
 )
 
-any(auth_did_not_coauth %in% auth_coauth)
-length(auth_did_not_coauth)
+any(auth_did_not_coauth %in% auth_coauth) # check
+
 length(auth_did_not_coauth) / 
     (length(auth_coauth) + length(auth_did_not_coauth)) * 100
 
+length(auth_did_not_coauth)
+
+
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# Section - Mean year of publication for co-authors
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
 df_bp <- rbind(df_coauth, df_did_not_coauth)
+df_bp$date <- as.integer(df_bp$date)
 df_bp$coauth <- factor(df_bp$coauth, levels=c("Coauthor", "Did not coauthor"))
 df_bp[, list(mean=mean(date), se=sd(date)/sqrt(.N), n=.N), by=coauth]
-
-# Plot
-plot_bp <- ggplot(df_bp) + 
-  geom_boxplot(aes(y=date, fill=as.character(coauth))) + 
-  theme +
-  ylab("Year of description\n") + 
-  theme(axis.title.x=element_blank(),
-      axis.text.x=element_blank(),
-      axis.ticks.x=element_blank()) + 
-  scale_fill_manual(name="", values=c("grey80", "grey40"))
-
-plot_bp
-# ggsave(paste0(dir_plot, 'fig-4.png'), plot_bp, units="cm", width=15, height=10, dpi=300)
 
 res <- t.test(
     df_coauth$date, df_did_not_coauth$date, 
     alternative = "two.sided", var.equal = FALSE
 )
 
-res$p.value
 res
+res$p.value
+
+# Plot
+plot_bp <- ggplot(df_bp) + 
+    geom_boxplot(aes(y=date, x=as.character(coauth))) + 
+    theme +
+    ylab("Year of description\n") + 
+    theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank()) + 
+    scale_fill_manual(name="", values=c("grey80", "grey40"))
+
+# plot_bp
+
+# ggsave(
+#     paste0(dir_plot, 'fig-X.png'), plot_bp, units="cm",
+#     width=15, height=10, dpi=300
+# )
+
+# res <- bartlett.test(df_coauth$date, df_did_not_coauth$date)
+# res
+
 
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 # Section - Proportion of authors across years
@@ -111,7 +134,7 @@ plot_auth_decade1 <- ggplot(data = df, aes(x=date.decade, fill=N)) +
         theme + scale_fill_grey()
 
 plot_auth_decade2 <- ggplot(data=df, aes(x=date.decade, fill=N)) +
-        geom_bar(stat='count') +
+        geom_bar(stat = 'count') +
         xlab("\nDecade") +
         ylab("Number of species with \nN number of authors\n") +
         theme + scale_fill_grey()
@@ -119,7 +142,15 @@ plot_auth_decade2 <- ggplot(data=df, aes(x=date.decade, fill=N)) +
 gr <- grid.arrange(plot_auth_decade1, plot_auth_decade2)
 
 ggsave(
-    paste0(dir_plot, 'fig-4.png'), gr, units="cm", width=30, height=15, dpi=300
+    paste0(dir_plot, 'fig-1.png'), gr, units="cm", width=30, height=15, dpi=300
 )
 
+dim(df_full[date.decade=="1820s"])
+df_full[date.decade=="1820s"]
+df_full[date.decade=="1820s", .N]
+df_full[date.decade=="1820s", .N, by="coauth"]
 
+
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# Section - Write data
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
