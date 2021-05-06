0d91b0f5b4d1c86c806dd436a6fe48aa2b1f8d2d
Eunice Soh <eunice.sjy@gmail.com>
2021-05-06 18:46:28 +0800

    [EJYS] Plot tax effort

--- 2020-08-31-jsa-type-v2-ch2/99-ancillary/plot-effort.r
+++ 2020-08-31-jsa-type-v2-ch2/99-ancillary/plot-effort.r
@@ -10,10 +10,14 @@ fdir <- paste0(dir_analysis_edie_model, model)
 
 df <- fread(paste0(fdir, 'offset.csv'), na.strings='')
 
-ggplot(df, aes(x=year, y=N)) + 
-    geom_line() + geom_smooth() + 
-    facet_wrap(group~.) + ggtitle("Taxonomic effort by publication") +
-    theme_minimal() + xlab("") + ylab("") 
+p <- ggplot(df, aes(x=year, y=N)) + 
+    geom_line() + 
+    facet_wrap(group~.) + ggtitle("") +
+    theme_minimal() + xlab("Year") + ylab("Number of publications\n") 
+
+ggsave(paste0(fdir, "te.png"), p, units="cm", width=20, height=10, dpi=300)
+
+
 
 
 model <- "version01/BGY-E2-C4-I8000-A0.8-T12-F25-V0/"
@@ -21,7 +25,68 @@ fdir <- paste0(dir_analysis_edie_model, model)
 
 df <- fread(paste0(fdir, 'offset.csv'), na.strings='')
 
-ggplot(df, aes(x=year, y=N)) + 
-    geom_line() + geom_smooth() + 
-    facet_wrap(group~.) + ggtitle("Taxonomic effort by describers") +
-    theme_minimal() + xlab("") + ylab("") 
+p <- ggplot(df, aes(x=year, y=N)) + 
+    geom_line() + 
+    facet_wrap(group~.) + ggtitle("") +
+    theme_minimal() + xlab("Year") + ylab("Number of describers\n") 
+
+ggsave(paste0(fdir, "te.png"), p, units="cm", width=20, height=10, dpi=300)
+
+
+# By family
+
+
+model <- "version01/FAM-E1-C4-I100000-A0.9-T12-F25-V0/"
+fdir <- paste0(dir_analysis_edie_model, model)
+
+df <- fread(paste0(fdir, 'offset.csv'), na.strings='')
+
+p <- ggplot(df, aes(x=year, y=N)) + 
+    geom_line() + 
+    facet_wrap(group~.) + ggtitle("") +
+    theme_minimal() + xlab("Year") + ylab("Number of publications\n") 
+
+ggsave(paste0(fdir, "te.png"), p, units="cm", width=20, height=10, dpi=300)
+
+
+model <- "version01/FAM-E2-C4-I8000-A0.8-T12-F25-V0/"
+fdir <- paste0(dir_analysis_edie_model, model)
+
+df <- fread(paste0(fdir, 'offset.csv'), na.strings='')
+
+p <- ggplot(df, aes(x=year, y=N)) + 
+    geom_line() + 
+    facet_wrap(group~.) + ggtitle("") +
+    theme_minimal() + xlab("Year") + ylab("Number of describers\n") 
+
+ggsave(paste0(fdir, "te.png"), p, units="cm", width=20, height=10, dpi=300)
+
+
+# By latitudinal grouping
+
+
+model <- "version01/LTY-E1-C4-I20000-A0.99-T12-F25-V0/"
+fdir <- paste0(dir_analysis_edie_model, model)
+
+df <- fread(paste0(fdir, 'offset.csv'), na.strings='')
+
+p <- ggplot(df, aes(x=year, y=N)) + 
+    geom_line() + 
+    facet_wrap(group~.) + ggtitle("") +
+    theme_minimal() + xlab("\nYear") + ylab("Number of publications\n") 
+
+ggsave(paste0(fdir, "te.png"), p, units="cm", width=24, height=10, dpi=300)
+
+
+
+model <- "version01/LTY-E2-C4-I100000-A0.99-T12-F25-V0/"
+fdir <- paste0(dir_analysis_edie_model, model)
+
+df <- fread(paste0(fdir, 'offset.csv'), na.strings='')
+
+p <- ggplot(df, aes(x=year, y=N)) + 
+    geom_line() + 
+    facet_wrap(group~.) + ggtitle("") +
+    theme_minimal() + xlab("\nYear") + ylab("Number of describers\n") 
+
+ggsave(paste0(fdir, "te.png"), p, units="cm", width=24, height=10, dpi=300)
